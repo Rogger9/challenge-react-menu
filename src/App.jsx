77320f41df663d1719893efd8e907bbd8d74c90e
Roger9 <gutierrez.rog12@gmail.com>
2022-03-10 19:43:41 -0300

    Refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Suspense, lazy } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useState } from 'react/cjs/react.development'
 import Loader from './components/Loader'
-import { useAuth } from './hooks/useAuth'
 
 const Home = lazy(() => import('./components/Home'))
 const RecipesDetail = lazy(() => import('./components/Recipes/RecipeDetail'))
@@ -9,16 +9,17 @@ const Login = lazy(() => import('./components/Login'))
 const PrivateRoute = lazy(() => import('./components/PrivateRoute'))
 const Page404 = lazy(() => import('./components/Page404'))
 
-function App () {
-  const { auth } = useAuth()
+const inicialState = () => window.localStorage.getItem('auth')
 
+function App () {
+  const [auth, setAuth] = useState(inicialState)
   return (
     <BrowserRouter>
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path='/' element={<PrivateRoute token={auth}><Home /></PrivateRoute>} />
           <Route path='/detail/:id' element={<PrivateRoute token={auth}><RecipesDetail /></PrivateRoute>} />
-          <Route path='/login' element={<Login />} />
+          <Route path='/login' element={<Login {...{ setAuth }} />} />
           <Route path='*' element={<Page404 />} />
         </Routes>
       </Suspense>
